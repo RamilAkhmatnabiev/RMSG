ad97bfa25e55add83eeb3f4fa9be82ebeb678d4c
RamilAkhmatnabiev <59731512+RamilAkhmatnabiev@users.noreply.github.com>
2020-06-09 13:24:07 +0300

    Initial commit

---
+++ app/src/main/java/com/example/rmsg/ViewHolder.java
@@ -0,0 +1,15 @@
+package com.example.rmsg;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+    TextView message;
+    public ViewHolder(View itemView) {
+        super(itemView);
+        message = itemView.findViewById(R.id.message_item);
+    }
+
+}
